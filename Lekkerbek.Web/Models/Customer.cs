63a5f37d26aaf5c4d63e6440aeaa895155c76804
taha <r0910951@ucll.be>
2023-02-26 16:45:46 +0100

    Creatie customer class (Model) #23

---
+++ Lekkerbek.Web/Models/Customer.cs
@@ -0,0 +1,33 @@
+﻿namespace Lekkerbek.Web.Models
+{
+    public Enum DishesList
+    {
+        vlees,
+            }
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        public string Address { get; set; } = string.Empty;
+
+        public DateTime Birthday { get; set; }
+
+        private bool loyaltyScore;
+        public bool LoyaltyScore { 
+            get { return loyaltyScore; } 
+            set 
+            { 
+                if (Orders.Count >= 3)
+                    loyaltyScore = true;
+                else
+                    loyaltyScore = false;
+            } }
+
+        public virtual ICollection <Order> Orders { get; set; }
+
+        
+
+        //public virtual ICollection<Dishes> PreferredDishes { get; set; }
+    }
+}
