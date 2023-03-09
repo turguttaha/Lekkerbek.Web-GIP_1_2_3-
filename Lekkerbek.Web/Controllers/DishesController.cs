d86350e4eb7860cc7c6b47c928d355a91095fff5
taha <r0910951@ucll.be>
2023-03-09 13:21:49 +0100

    dishes verniewen

---
+++ Lekkerbek.Web/Controllers/DishesController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Lekkerbek.Web.Data;
+using Lekkerbek.Web.Models;
+
+namespace Lekkerbek.Web.Controllers
+{
+    public class DishesController : Controller
+    {
+        private readonly LekkerbekContext _context;
+
+        public DishesController(LekkerbekContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Dishes
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.Dishes.ToListAsync());
+        }
+
+        // GET: Dishes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Dishes == null)
+            {
+                return NotFound();
+            }
+
+            var dish = await _context.Dishes
+                .FirstOrDefaultAsync(m => m.DishId == id);
+            if (dish == null)
+            {
+                return NotFound();
+            }
+
+            return View(dish);
+        }
+
+        // GET: Dishes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Dishes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("DishId,Name,Description,Price")] Dish dish)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(dish);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(dish);
+        }
+
+        // GET: Dishes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Dishes == null)
+            {
+                return NotFound();
+            }
+
+            var dish = await _context.Dishes.FindAsync(id);
+            if (dish == null)
+            {
+                return NotFound();
+            }
+            return View(dish);
+        }
+
+        // POST: Dishes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("DishId,Name,Description,Price")] Dish dish)
+        {
+            if (id != dish.DishId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(dish);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DishExists(dish.DishId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(dish);
+        }
+
+        // GET: Dishes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Dishes == null)
+            {
+                return NotFound();
+            }
+
+            var dish = await _context.Dishes
+                .FirstOrDefaultAsync(m => m.DishId == id);
+            if (dish == null)
+            {
+                return NotFound();
+            }
+
+            return View(dish);
+        }
+
+        // POST: Dishes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Dishes == null)
+            {
+                return Problem("Entity set 'LekkerbekContext.Dishes'  is null.");
+            }
+            var dish = await _context.Dishes.FindAsync(id);
+            if (dish != null)
+            {
+                _context.Dishes.Remove(dish);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DishExists(int id)
+        {
+          return _context.Dishes.Any(e => e.DishId == id);
+        }
+    }
+}
--- Lekkerbek.Web/Models/Dish.cs
+++ Lekkerbek.Web/Models/Dish.cs
@@ -6,7 +6,6 @@
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public double Price { get; set; }
-        public int Discount { get; set; }
         public virtual ICollection<OrderLine> OrderLines { get; set; }
 
     }
