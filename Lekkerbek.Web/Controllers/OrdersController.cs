0553056b041fe4bec1e791e07aaef93c4ff1093d
taha <r0910951@ucll.be>
2023-03-15 21:33:21 +0100

    Een administratief bediende moet een bestelling kunnen wijzigen tot een uur voor het tijdslot

--- Lekkerbek.Web/Controllers/OrdersController.cs
+++ Lekkerbek.Web/Controllers/OrdersController.cs
@@ -40,24 +40,12 @@ namespace Lekkerbek.Web.Controllers
                 .Include(o => o.TimeSlot)
                 .FirstOrDefaultAsync(m => m.OrderID == id);
 
-            //filtering orderlines occording to orderId
-             List<OrderLine> allOrderLines = _context.OrderLines.ToList();
-             List<OrderLine> filteredOrderLines = new List<OrderLine>();
-
-                foreach (var orderLine in allOrderLines.Where(c => c.OrderID == id))
-                {
-                    if (!filteredOrderLines.Contains(orderLine))
-                    filteredOrderLines.Add(orderLine);
-                }
-
-            
-            ViewBag.listOfTheOrder  = filteredOrderLines;
-
-
             if (order == null)
             {
                 return NotFound();
             }
+            ViewBag.listOfTheOrder  = FilterOrderLines(id);
+
 
             return View(order);
         }
@@ -229,7 +217,8 @@ namespace Lekkerbek.Web.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult AddOrderLine([Bind("OrderLineID,ExtraDetails,DishAmount,OrderID,DishID")] OrderLine orderLine)
         {
-
+            // go to database and get dish name via id
+            orderLine.Dish = _context.Dishes.Find(orderLine.DishID);
             Order.TemproraryCart.Add(orderLine);
             ViewData["Message"] = "Your Dish is added";
             ViewBag.TemproraryCart = Order.TemproraryCart;
@@ -272,31 +261,128 @@ namespace Lekkerbek.Web.Controllers
             return RedirectToAction("index", "Orders");
         }
 
-        // GET: Orders/Edit/5
-        public async Task<IActionResult> Edit(int? id)
+        // GET: Orders/EditOrder/5
+        public async Task<IActionResult> EditOrder(int? id)
         {
+          
+
             if (id == null || _context.Orders == null)
             {
                 return NotFound();
             }
 
             var order = await _context.Orders.FindAsync(id);
+
             if (order == null)
             {
                 return NotFound();
             }
-            //ViewData["CustomerID"] = new SelectList(_context.Customers, "CustomerId", "FName");
-            ViewData["CustomerID"] = new SelectList(_context.Customers, "CustomerId","FName", order.CustomerID);
-            ViewData["TimeSlotID"] = new SelectList(_context.TimeSlots, "Id", "StartTimeSlot", order.TimeSlotID);
-            return View(order);
+
+            var timeSlotItem = await _context.TimeSlots.FindAsync(order.TimeSlotID);
+            DateTime startTimeSlot = timeSlotItem.StartTimeSlot;
+            DateTime endTimeSlot = startTimeSlot.AddMinutes(15);
+            DateTime now = DateTime.Now;
+            DateTime twoHoursAgo = endTimeSlot.AddMinutes(-60);
+
+            if (twoHoursAgo > now)
+            {
+                ViewData["CustomerID"] = new SelectList(_context.Customers, "CustomerId", "FName", order.CustomerID);
+
+                var timeSlot = await _context.TimeSlots.FindAsync(order.TimeSlotID);
+
+                TempData["SelectDate"] = timeSlot.StartTimeSlot.ToString("yyyy-MM-dd");
+                TempData["time"] = timeSlot.StartTimeSlot.ToString("H:mm");
+
+                List<SelectListItem> TimeSlotsSelectList = new List<SelectListItem>() {
+                new SelectListItem {
+                    Text = "12:00", Value = "12:00"
+                },
+                new SelectListItem {
+                    Text = "12:15", Value = "12:15"
+                },
+                new SelectListItem {
+                    Text = "12:30", Value = "12:30"
+                },
+                new SelectListItem {
+                    Text = "12:45", Value = "12:45"
+                },
+                new SelectListItem {
+                    Text = "13:00", Value = "13:00"
+                },
+                new SelectListItem {
+                    Text = "13:15", Value = "13:15"
+                },
+                new SelectListItem {
+                    Text = "13:30", Value = "13:30"
+                },
+                new SelectListItem {
+                    Text = "13:45", Value = "13:45"
+                },
+                new SelectListItem {
+                    Text = "18:00", Value = "18:00"
+                },
+                new SelectListItem {
+                    Text = "18:15", Value = "18:15"
+                },
+                new SelectListItem {
+                    Text = "18:30", Value = "18:30"
+                },
+                new SelectListItem {
+                    Text = "18:45", Value = "18:45"
+                },
+                new SelectListItem {
+                    Text = "19:00", Value = "19:00"
+                },
+                new SelectListItem {
+                    Text = "19:15", Value = "19:15"
+                },
+                new SelectListItem {
+                    Text = "19:30", Value = "19:30"
+                },
+                new SelectListItem {
+                    Text = "19:45", Value = "19:45"
+                },
+                new SelectListItem {
+                    Text = "20:00", Value = "20:00"
+                },
+                new SelectListItem {
+                    Text = "20:15", Value = "20:15"
+                },
+                new SelectListItem {
+                    Text = "20:30", Value = "20:30"
+                },
+                new SelectListItem {
+                    Text = "20:45", Value = "20:45"
+                },
+                new SelectListItem {
+                    Text = "21:00", Value = "21:00"
+                },
+                new SelectListItem {
+                    Text = "21:15", Value = "21:15"
+                },
+                new SelectListItem {
+                    Text = "21:30", Value = "21:30"
+                },
+                new SelectListItem {
+                    Text = "21:45", Value = "21:45"
+                },
+            };
+                ViewBag.TimeSlotsSelectList = TimeSlotsSelectList;
+
+                ViewBag.listOfTheOrder = FilterOrderLines(id);
+
+                return View(order);
+            }
+            TempData["TimesPast"] = "Order has less than 1 hours to prepare so no changes can be made to the order.";
+            return RedirectToAction("Index", "Orders");
         }
 
-        // POST: Orders/Edit/5
+        // POST: Orders/EditOrder/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("OrderID,Finished,CustomerID,TimeSlotID")] Order order)
+        public async Task<IActionResult> EditOrder(int id, [Bind("OrderID,Finished,CustomerID")] Order order,IFormCollection collection)
         {
             if (id != order.OrderID)
             {
@@ -307,7 +393,17 @@ namespace Lekkerbek.Web.Controllers
             //{
                 try
                 {
-                    _context.Update(order);
+
+                TimeSlot timeSlot = new TimeSlot();               
+                string x = collection["TimeSlotsSelectList"];
+                String selectedDate = collection["TimeSlotID"] + " " + x;
+                DateTime timeSlotDateAndTime = Convert.ToDateTime(selectedDate);
+                timeSlot.StartTimeSlot = timeSlotDateAndTime;
+                _context.Add(timeSlot);
+                await _context.SaveChangesAsync();
+                var lastTimeSlot = _context.TimeSlots.OrderByDescending(t => t.Id).FirstOrDefault();
+                order.TimeSlotID = lastTimeSlot.Id;
+                _context.Update(order);
                     await _context.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
@@ -321,11 +417,153 @@ namespace Lekkerbek.Web.Controllers
                         throw;
                     }
                 }
-                return RedirectToAction(nameof(Index));
+
+            TempData["OrderId"] = order.OrderID;
+            ViewData["CustomerID"] = new SelectList(_context.Customers, "CustomerId", "FName", order.CustomerID);
+
+            var timeSlots = await _context.TimeSlots.FindAsync(order.TimeSlotID);
+
+            TempData["SelectDate"] = timeSlots.StartTimeSlot.ToString("yyyy-MM-dd");
+            TempData["time"] = timeSlots.StartTimeSlot.ToString("H:mm");
+
+            List<SelectListItem> TimeSlotsSelectList = new List<SelectListItem>() {
+                new SelectListItem {
+                    Text = "12:00", Value = "12:00"
+                },
+                new SelectListItem {
+                    Text = "12:15", Value = "12:15"
+                },
+                new SelectListItem {
+                    Text = "12:30", Value = "12:30"
+                },
+                new SelectListItem {
+                    Text = "12:45", Value = "12:45"
+                },
+                new SelectListItem {
+                    Text = "13:00", Value = "13:00"
+                },
+                new SelectListItem {
+                    Text = "13:15", Value = "13:15"
+                },
+                new SelectListItem {
+                    Text = "13:30", Value = "13:30"
+                },
+                new SelectListItem {
+                    Text = "13:45", Value = "13:45"
+                },
+                new SelectListItem {
+                    Text = "18:00", Value = "18:00"
+                },
+                new SelectListItem {
+                    Text = "18:15", Value = "18:15"
+                },
+                new SelectListItem {
+                    Text = "18:30", Value = "18:30"
+                },
+                new SelectListItem {
+                    Text = "18:45", Value = "18:45"
+                },
+                new SelectListItem {
+                    Text = "19:00", Value = "19:00"
+                },
+                new SelectListItem {
+                    Text = "19:15", Value = "19:15"
+                },
+                new SelectListItem {
+                    Text = "19:30", Value = "19:30"
+                },
+                new SelectListItem {
+                    Text = "19:45", Value = "19:45"
+                },
+                new SelectListItem {
+                    Text = "20:00", Value = "20:00"
+                },
+                new SelectListItem {
+                    Text = "20:15", Value = "20:15"
+                },
+                new SelectListItem {
+                    Text = "20:30", Value = "20:30"
+                },
+                new SelectListItem {
+                    Text = "20:45", Value = "20:45"
+                },
+                new SelectListItem {
+                    Text = "21:00", Value = "21:00"
+                },
+                new SelectListItem {
+                    Text = "21:15", Value = "21:15"
+                },
+                new SelectListItem {
+                    Text = "21:30", Value = "21:30"
+                },
+                new SelectListItem {
+                    Text = "21:45", Value = "21:45"
+                },
+            };
+            ViewBag.TimeSlotsSelectList = TimeSlotsSelectList;
+
+            ViewBag.listOfTheOrder = FilterOrderLines(id);
+            return View(order);
             //}
             ViewData["CustomerID"] = new SelectList(_context.Customers, "CustomerId", "Name", order.CustomerID);
             ViewData["TimeSlotID"] = new SelectList(_context.TimeSlots, "Id", "Id", order.TimeSlotID);
             return View(order);
+        }
+
+        // GET: OrderLines/Edit/5
+        public async Task<IActionResult> EditOrderLine(int? id)
+        {
+            if (id == null || _context.OrderLines == null)
+            {
+                return NotFound();
+            }
+
+            var orderLine = await _context.OrderLines.FindAsync(id);
+            if (orderLine == null)
+            {
+                return NotFound();
+            }
+            ViewData["DishID"] = new SelectList(_context.Dishes, "DishId", "Name", orderLine.DishID);
+            //ViewData["OrderID"] = new SelectList(_context.Orders, "OrderID", "OrderID", orderLine.OrderID);
+            return View(orderLine);
+        }
+
+        // POST: OrderLines/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditOrderLine(int id, [Bind("OrderLineID,ExtraDetails,DishAmount,OrderID,DishID")] OrderLine orderLine)
+        {
+            if (id != orderLine.OrderLineID)
+            {
+                return NotFound();
+            }
+
+            // if (ModelState.IsValid)
+            //{
+            try
+                {
+                
+                    _context.Update(orderLine);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                //if (!OrderLineExists(orderLine.OrderLineID))
+                //{
+                //    return NotFound();
+                //}
+                //else
+                //{
+                //    throw;
+                //}
+            }
+            return RedirectToAction("EditOrder", new { id = orderLine.OrderID });
+            //}
+            ViewData["DishID"] = new SelectList(_context.Dishes, "DishId", "DishId", orderLine.DishID);
+            ViewData["OrderID"] = new SelectList(_context.Orders, "OrderID", "OrderID", orderLine.OrderID);
+            return View(orderLine);
         }
 
         // GET: Orders/Delete/5
@@ -340,16 +578,8 @@ namespace Lekkerbek.Web.Controllers
                 .Include(o => o.Customer)
                 .Include(o => o.TimeSlot)
                 .FirstOrDefaultAsync(m => m.OrderID == id);
-            //filtering orderlines occording to orderId
-            List<OrderLine> allOrderLines = _context.OrderLines.ToList();
-            List<OrderLine> filteredOrderLines = new List<OrderLine>();
 
-            foreach (var orderLine in allOrderLines.Where(c => c.OrderID == id))
-            {
-                if (!filteredOrderLines.Contains(orderLine))
-                    filteredOrderLines.Add(orderLine);
-            }
-            ViewBag.listOfTheOrder = filteredOrderLines;
+            ViewBag.listOfTheOrder = FilterOrderLines(id);
 
             if (order == null)
             {
@@ -379,18 +609,10 @@ namespace Lekkerbek.Web.Controllers
                 DateTime endTimeSlot = startTimeSlot.AddMinutes(15);
                 DateTime now = DateTime.Now;
                 DateTime twoHoursAgo = endTimeSlot.AddMinutes(-120);
-                //filtering orderlines occording to orderId
-                List<OrderLine> allOrderLines = _context.OrderLines.ToList();
-                List<OrderLine> filteredOrderLines = new List<OrderLine>();
 
-                foreach (var orderLine in allOrderLines.Where(c => c.OrderID == id))
-                {
-                    if (!filteredOrderLines.Contains(orderLine))
-                        filteredOrderLines.Add(orderLine);
-                }
+                List<OrderLine> filteredOrderLines = FilterOrderLines(id);
+
                 if (twoHoursAgo > now) {
-
-
                 
                     //deleting order timeslot orderlines from database
                     if (timeSlot != null && filteredOrderLines != null)
@@ -414,6 +636,27 @@ namespace Lekkerbek.Web.Controllers
         private bool OrderExists(int id)
         {
           return _context.Orders.Any(e => e.OrderID == id);
+        }
+
+        private List<OrderLine> FilterOrderLines(int? Orderid)
+        {
+            if (Orderid == null )
+            {
+                return new List<OrderLine>();
+            }
+            //filtering orderlines occording to orderId
+            List<OrderLine> allOrderLines = _context.OrderLines.ToList();
+            List<OrderLine> filteredOrderLines = new List<OrderLine>();
+            foreach (var orderLine in allOrderLines.Where(c => c.OrderID == Orderid))
+            {
+                if (!filteredOrderLines.Contains(orderLine))
+                {
+                    orderLine.Dish = _context.Dishes.Find(orderLine.DishID);
+                    filteredOrderLines.Add(orderLine);                  
+                }
+                    
+            }
+            return filteredOrderLines;
         }
     }
 }
