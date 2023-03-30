872589c05842c2ec3ba1e8ba2192732b8ac3e180
BitBucketSora <sorainokinawa@gmail.com>
2023-03-30 11:34:06 +0200

    Kendo Grid. Still need add button

--- Lekkerbek.Web/Services/ProductService.cs
+++ Lekkerbek.Web/Services/ProductService.cs
@@ -10,180 +10,114 @@ using System.Web;
 
 namespace Lekkerbek.Web.NewFolder
 {
-        public class ProductService : IDisposable
+    public class ProductService
+    {
+        private static bool UpdateDatabase = true;//If true, stores info in the database. If false, session.
+
+        private LekkerbekContext _context;
+
+        public ProductService(LekkerbekContext entities)
         {
-            private static bool UpdateDatabase = false;
+            this._context = entities;
+        }
+
+        private IList<Customer> GetAll()
+        {
+
+            var result = _context.Customers.Select(customer => new Customer
+            //This is another way to make a new object
+            {
+                CustomerId = customer.CustomerId,
+                FName = customer.FName,
+                LName = customer.LName,
+                Email = customer.Email,
+                PhoneNumber = customer.PhoneNumber,
+                Address = customer.Address,
+                Birthday = customer.Birthday,
+                PreferredDishId = customer.PreferredDishId,
+                PreferredDish = new PreferredDish()
+                {
+                    PreferredDishId = customer.PreferredDish.PreferredDishId,
+                    Name = customer.PreferredDish.Name
+                }
+
+            }).ToList();
+
+            return result;
+        }
+
+        public IEnumerable<Customer> Read()
+        {
+            return GetAll();
+        }
+
+        public void Create(Customer customer)
+        {
+            var customer1 = new Customer();//Declaring new customer entity and sticking in the info given from View.
+
+            customer1.CustomerId = customer.CustomerId;
+            customer1.FName = customer.FName;
+            customer1.LName = customer.LName;
+            customer1.Email = customer.Email;
+            customer1.PhoneNumber = customer.PhoneNumber;
+            customer1.Address = customer.Address;
+            customer1.Birthday = customer.Birthday;
+            customer1.PreferredDishId = customer.PreferredDishId;
+            //customer1.PreferredDish = new PreferredDish();
+
+
+            if (customer1.PreferredDishId == null)
+            {
+                customer1.PreferredDishId = 1;
+            }
+
+            else
+            {
+                customer1.PreferredDishId = customer.PreferredDish.PreferredDishId;
+            }
+
+            _context.Customers.Add(customer1);
+            _context.SaveChanges();
+
+            customer.CustomerId = customer1.CustomerId;
+        }
+
+        public void Update(Customer customer)
+        {
+            var customer1 = new Customer();
+
+            customer1.CustomerId = customer.CustomerId;
+            customer1.FName = customer.FName;
+            customer1.LName = customer.LName;
+            customer1.Email = customer.Email;
+            customer1.PhoneNumber = customer.PhoneNumber;
+            customer1.Address = customer.Address;
+            customer1.Birthday = customer.Birthday;
+            customer1.PreferredDishId = customer.PreferredDishId;
+
+            if (customer.PreferredDish != null)
+            {
+                customer1.CustomerId = customer.CustomerId;
+            }
+
+            _context.Customers.Attach(customer1);
+            _context.Entry(customer1).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+    } }
+    
             
-            private LekkerbekContext _context;
-
-            public ProductService(LekkerbekContext entities)
-            {
-                this._context = entities;
-            }
-            
-            public IList<Customer> GetAll()
-            {
-               // var result = HttpContext.Session["Products"] as IList<Customer>;
-
-                //if (result == null || UpdateDatabase)
-               // {
-                    var result = _context.Customers.Select(customer => new Customer
-                    //This is another way to make a new object
-                    {
-                        CustomerId = customer.CustomerId,
-                        FName = customer.FName,
-                        LName = customer.LName,
-                        Email = customer.Email,
-                        PhoneNumber = customer.PhoneNumber,
-                        Address = customer.Address,
-                        Birthday = customer.Birthday,
-                        PreferredDishId = customer.PreferredDishId,
-                        PreferredDish = new PreferredDish()
-                        {
-                            PreferredDishId = customer.PreferredDish.PreferredDishId,
-                            Name = customer.PreferredDish.Name
-                        }
-                        
-                    }).ToList();
-
-                   // HttpContext.Session["Products"] = result;
-              //  }
-
-                return result;
-            }
-
-            public IEnumerable<Customer> Read()
-            {
-                return GetAll();
-            }
-
-            public void Create(Customer customer)
-            {
-                //if (!UpdateDatabase)
-                //{
-               // var first = GetAll().OrderByDescending(e => e.CustomerId).FirstOrDefault();
-               //  var id = (first != null) ? first.CustomerId : 0;
-
-                // customer.CustomerId = id + 1;
-
-                //    if (customer.PreferredDishId == null)
-                //    {
-                //        customer.PreferredDishId = 1;
-                //    }
-
-                //    if (customer.PreferredDishId == null)
-                //    {
-                //        customer.PreferredDishId = new PreferredDish() { PreferredDishId = 1, Name = "Beverages" };
-                //    }
-
-                //    GetAll().Insert(0, customer);
-                //}
-                //else
-                //{
-                    var entity = new Customer();//Declaring new customer entity and sticking in the info given from View.
-
-                    entity.FName = customer.FName;
-                    entity.Address = customer.Address;
-                    entity.LName = customer.LName;
-                    entity.PhoneNumber = customer.PhoneNumber;
-                    entity.Birthday = customer.Birthday;
-                    entity.Email = customer.Email;
-
-                    if (entity.PreferredDishId == null)
-                    {
-                        entity.PreferredDishId = 1;
-                    }
-
-                    if (customer.PreferredDishId != null)
-                    {
-                        entity.PreferredDishId = customer.PreferredDish.PreferredDishId;
-                    }
-
-                    _context.Customers.Add(entity);
-                    _context.SaveChanges();
-
-                    customer.CustomerId = entity.CustomerId;
-               // }
-            }
-        /*
-            public void Update(ProductViewModel product)
-            {
-                if (!UpdateDatabase)
+               /* public void Destroy(Customer customer)
                 {
-                    var target = One(e => e.ProductID == product.ProductID);
+                    var customer1 = new Customer();
 
-                    if (target != null)
-                    {
-                        target.ProductName = product.ProductName;
-                        target.UnitPrice = product.UnitPrice;
-                        target.UnitsInStock = product.UnitsInStock;
-                        target.Discontinued = product.Discontinued;
+                    customer1.CustomerId = customer.CustomerId;
 
-                        if (product.CategoryID == null)
-                        {
-                            product.CategoryID = 1;
-                        }
+                    _context.customer.Attach(customer1);
 
-                        if (product.Category != null)
-                        {
-                            product.CategoryID = product.Category.CategoryID;
-                        }
-                        else
-                        {
-                            product.Category = new CategoryViewModel()
-                            {
-                                CategoryID = (int)product.CategoryID,
-                                CategoryName = _context.Categories.Where(s => s.CategoryID == product.CategoryID).Select(s => s.CategoryName).First()
-                            };
-                        }
+                    _context.Products.Remove(customer1);
 
-                        target.CategoryID = product.CategoryID;
-                        target.Category = product.Category;
-                    }
-                }
-                else
-                {
-                    var entity = new Product();
-
-                    entity.ProductID = product.ProductID;
-                    entity.ProductName = product.ProductName;
-                    entity.UnitPrice = product.UnitPrice;
-                    entity.UnitsInStock = (short)product.UnitsInStock;
-                    entity.Discontinued = product.Discontinued;
-                    entity.CategoryID = product.CategoryID;
-
-                    if (product.Category != null)
-                    {
-                        entity.CategoryID = product.Category.CategoryID;
-                    }
-
-                    _context.Products.Attach(entity);
-                    _context.Entry(entity).State = EntityState.Modified;
-                    _context.SaveChanges();
-                }
-            }
-
-            public void Destroy(ProductViewModel product)
-            {
-                if (!UpdateDatabase)
-                {
-                    var target = GetAll().FirstOrDefault(p => p.ProductID == product.ProductID);
-                    if (target != null)
-                    {
-                        GetAll().Remove(target);
-                    }
-                }
-                else
-                {
-                    var entity = new Product();
-
-                    entity.ProductID = product.ProductID;
-
-                    _context.Products.Attach(entity);
-
-                    _context.Products.Remove(entity);
-
-                    var orderDetails = _context.Order_Details.Where(pd => pd.ProductID == entity.ProductID);
+                    var orderDetails = _context.Order_Details.Where(pd => pd.ProductID == customer1.c);
 
                     foreach (var orderDetail in orderDetails)
                     {
@@ -192,17 +126,19 @@ namespace Lekkerbek.Web.NewFolder
 
                     _context.SaveChanges();
                 }
-            }
-        */
-            public Customer One(Func<Customer, bool> predicate)
+                }
+            
+            /*Customer One(Func<Customer, bool> predicate)
             {
                 return GetAll().FirstOrDefault(predicate);
-            }
+            }*/
+        
 
-            public void Dispose()
+            /*public void Dispose() //why not @override?
             {
-                _context.Dispose();
-            }
-        }
-    }
+                entities.Dispose();
+            }*/
+
+
+    
 
