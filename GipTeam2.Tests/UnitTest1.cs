8e97c87d6bd5a1efb29592508ca43ea61d7e6739
Taha Turgut <r0910951@ucll.be>
2023-06-03 15:57:03 +0200

    #277

---
+++ GipTeam2.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace GipTeam2.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Gipteam2_Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Gipteam2_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
